cb5c2776a9df041be9d4e53e7c395a8c877423bd
BerkanGH <berkankeskin28@gmail.com>
2024-10-02 15:42:18 +0300

    patika ilk projesi
    Neden patikayı seçtiğim

---
+++ İlk uygulama/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace ilkuygulama { 
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Berkan Keskin");
+            Console.WriteLine("Kendimi daha iyi geliştirebilmek için patika programına katıldım");
+        }
+    }
+}
+
